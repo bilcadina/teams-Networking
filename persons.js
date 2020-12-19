43af2cb9d36041cb29c80ad1a2ae5e150a02a660
bilcadina <adina15felicia@yahoo.com>
2020-12-19 13:22:49 +0200

    load members from jscript

--- persons.js
+++ persons.js
@@ -1,19 +1,45 @@
 
+// function insertPersons(members){
+//     const tbody = document.querySelector("#persons tbody");
+
+//     var membersHTML = '';
+//     members.forEach(member => {
+//         console.log(member.firstName, member.lastName, member.link);
+//         membersHTML += getPersonHtml(member.firstName, member.lastName, member.link);
+//     });
+//     tbody.innerHTML =membersHTML;
+// }
+
 function insertPersons(members){
-    const tbody= document.querySelector("#list tbody");
+    const tbody = document.querySelector("#persons tbody");
 
-    console.log(tbody);
-
-    tbody.innerHTML= getPersonsHtml(members);
-
+     var membersHTML = members.map(member => {
+        return getPersonHtml(member.firstName, member.lastName, member.link, member.age);
+    });
+    console.log('members: ', membersHTML);
+    tbody.innerHTML = membersHTML.join('');
 }
 
-function getPersonsHTML{
-
+function getPersonHtml(firstName,lastName,link, age)
+{
+    return `<tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td><a target="_blank" href="https://www.linkedin.com/feed/${link}">linkedin</a></td>
+    <td>${age}</td>
+ </tr>`
 }
 
+function getTeamMembers(){
+    fetch('team.json')
+    .then(response => response.json())
+    .then(members => {
+        console.log('datele mele: ', members)
+        insertPersons(members);
+    });
+}
 
-insertPersons();
+    getTeamMembers();
 
 //todo map 
 
