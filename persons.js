e6a3ed9945c963506bd0f4c5e065d98cf7b3d9bf
bilcadina <adina15felicia@yahoo.com>
2020-12-19 11:15:16 +0200

    teamNetworking

---
+++ persons.js
@@ -0,0 +1,19 @@
+
+function insertPersons(members){
+    const tbody= document.querySelector("#list tbody");
+
+    console.log(tbody);
+
+    tbody.innerHTML= getPersonsHtml(members);
+
+}
+
+function getPersonsHTML{
+
+}
+
+
+insertPersons();
+
+//todo map 
+
