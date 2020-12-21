73fd2f4941e7a574e9b3967012c6602116701c18
bilcadina <adina15felicia@yahoo.com>
2020-12-21 18:55:12 +0200

    final team networking

--- persons.js
+++ persons.js
@@ -1,32 +1,21 @@
 
-// function insertPersons(members){
-//     const tbody = document.querySelector("#persons tbody");
-
-//     var membersHTML = '';
-//     members.forEach(member => {
-//         console.log(member.firstName, member.lastName, member.link);
-//         membersHTML += getPersonHtml(member.firstName, member.lastName, member.link);
-//     });
-//     tbody.innerHTML =membersHTML;
-// }
-
 function insertPersons(members){
     const tbody = document.querySelector("#persons tbody");
 
      var membersHTML = members.map(member => {
-        return getPersonHtml(member.firstName, member.lastName, member.link, member.age);
+        return getPersonHtml(member.firstName, member.lastName, member.link, member.telephone);
     });
     console.log('members: ', membersHTML);
     tbody.innerHTML = membersHTML.join('');
 }
 
-function getPersonHtml(firstName,lastName,link, age)
+function getPersonHtml(firstName,lastName,link,telephone)
 {
     return `<tr>
     <td>${firstName}</td>
     <td>${lastName}</td>
     <td><a target="_blank" href="https://www.linkedin.com/feed/${link}">linkedin</a></td>
-    <td>${age}</td>
+    <td>${telephone}</td>
  </tr>`
 }
 
