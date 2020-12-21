37b5f915074f2b2861a9fc69a74bfffd1d967518
bilcadina <adina15felicia@yahoo.com>
2020-12-21 19:33:59 +0200

    fix getPersonsHtml

--- persons.js
+++ persons.js
@@ -2,33 +2,25 @@
 function insertPersons(members){
     const tbody = document.querySelector("#persons tbody");
 
-     var membersHTML = members.map(member => {
-        return getPersonHtml(member.firstName, member.lastName, member.link, member.telephone);
+     var membersHTML = members.map(function(member) {
+        return getPersonHtml(member.firstName, member.lastName, member.link);
     });
-    console.log('members: ', membersHTML);
     tbody.innerHTML = membersHTML.join('');
 }
 
-function getPersonHtml(firstName,lastName,link,telephone)
-{
+function getPersonHtml(firstName,lastName,link){
     return `<tr>
     <td>${firstName}</td>
     <td>${lastName}</td>
     <td><a target="_blank" href="https://www.linkedin.com/feed/${link}">linkedin</a></td>
-    <td>${telephone}</td>
  </tr>`
 }
 
 function getTeamMembers(){
     fetch('team.json')
     .then(response => response.json())
-    .then(members => {
-        console.log('datele mele: ', members)
-        insertPersons(members);
+    .then(members => { insertPersons(members);
     });
 }
-
     getTeamMembers();
-
-//todo map 
 
