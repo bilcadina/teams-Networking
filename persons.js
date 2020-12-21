a5ec725eabcaeef33b7949e2a15a5b2c3b6f8e9a
bilcadina <adina15felicia@yahoo.com>
2020-12-21 21:13:02 +0200

    search by name

--- persons.js
+++ persons.js
@@ -13,12 +13,30 @@ function getPersonHtml(firstName,lastName,link){
     <td><a target="_blank" href="https://www.linkedin.com/feed/${link}">linkedin</a></td>
  </tr>`
 }
+    let allPersons = [];
 
 function getTeamMembers(){
     fetch('team.json')
     .then(response => response.json())
-    .then(members => { insertPersons(members);
+    .then(data => { 
+        allPersons = data;
+        insertPersons(data);
     });
 }
-    getTeamMembers();
+  function searchPersons(text){
+      console.warn('search',text,allPersons)
+      return allPersons.filter(person => {
+          console.info(person.firstName);
+          return person.firstName == text;
+      });
+  } 
 
+    const search = document.getElementById('search');
+    //const search = document.querySelector('#search'); pui diez la query dar se pot chema in acelasi lucru
+    
+    search.addEventListener('input', e => {
+        const text = e.target.value;
+        const filtrate = searchPersons(text);
+    })
+
+    getTeamMembers();
