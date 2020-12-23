8bf5f7c720f20e341e393b260d09959402087eea
bilcadina <adina15felicia@yahoo.com>
2020-12-23 22:13:20 +0200

    fixed erros

--- persons.js
+++ persons.js
@@ -1,42 +1,47 @@
 
-function insertPersons(members){
-    const tbody = document.querySelector("#persons tbody");
-     var membersHTML = members.map(member => 
-         getPersonHtml(member.firstName, member.lastName, member.link));
-    tbody.innerHTML = membersHTML.join('');
+function insertPersons(persons) {
+    const tbody = document.querySelector("#list tbody");
+    tbody.innerHTML = getPersonsHtml(persons);
 }
 
-function getPersonHtml(firstName,lastName,link){
+function getPersonsHtml (persons) {
+    return persons.map(getPersonHtml).join("");
+}
+
+function getPersonHtml (person) {
+    const linkedin = person.linkedin;
     return `<tr>
-    <td>${firstName}</td>
-    <td>${lastName}</td>
-    <td><a target="_blank" href="https://www.linkedin.com/feed/${link}">linkedin</a></td>
- </tr>`
+        <td>${person.firstName}</td>
+        <td>${person.lastName}</td>
+        <td><a target="_blank" href="https://www.linkedin.com/feed/${linkedin}">linkedin</a></td>
+ </tr>`;
 }
     let allPersons = [];
 
-function getTeamMembers(){
-    fetch('team.json')
+
+fetch('team.json')
     .then(response => response.json())
     .then(data => { 
         allPersons = data;
         insertPersons(data);
     });
-}
-  function searchPersons(text){
-      console.warn('search',text,allPersons)
-      return allPersons.filter(person => {
-          console.info(person.firstName);
-          return person.firstName == text;
-      });
-  } 
 
+function searchPersons(text) {
+      text = text.toLowerCase();
+      console.warn("search", text);
+    return allPersons.filter(person => {
+          console.info(person.firstName);
+        return person.firstName.toLowerCase().indexOf(text) > -1 || 
+          person.lastName.toLowerCase().indexOf(text) > -1;
+    });
+  }  
     const search = document.getElementById('search');
-    //const search = document.querySelector('#search'); pui diez la query dar se pot chema in acelasi lucru
-    
     search.addEventListener('input', e => {
         const text = e.target.value;
-        const filtrate = searchPersons(text);
-    })
 
-    getTeamMembers();
+        const filtrate = searchPersons(text);
+        console.info({filtrate})
+    
+
+    insertPersons(filtrate);
+});
